7d93b461ecc09c27b1b075d1843cdb656d70d4f6
HoshiHamza <55294741+HoshiHamza@users.noreply.github.com>
2020-07-05 19:49:18 +0500

    Add files via upload

---
+++ build/precache-manifest.f6e881c576d3ccfb7c43e64ab9da2981.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3755fa295083e920baef46f6ecb2b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e78b8e1a57dfb72e894",
+    "url": "/static/css/main.63a9cda9.chunk.css"
+  },
+  {
+    "revision": "f81c546e30c2b14a7b2f",
+    "url": "/static/js/2.302d5a57.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.302d5a57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e78b8e1a57dfb72e894",
+    "url": "/static/js/main.710668c5.chunk.js"
+  },
+  {
+    "revision": "3cf8ae71a09948a08a2e",
+    "url": "/static/js/runtime-main.4857cacc.js"
+  }
+]);
